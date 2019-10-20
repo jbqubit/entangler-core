cb7e89a0ff950ade8e0d6bed8f633ae9d08862cd
Drew Risinger <drewrisinger@users.noreply.github.com>
2019-10-20 00:25:33 -0400

    setup: housekeeping.
    Fix formatting, add pytest, URL for project, and instructions.

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from setuptools import setup, find_packages
+"""Install the ``entangler`` package for use in ARTIQ.
 
-setup(
-    name='entangler',
-    version='0.1',
-    packages=find_packages(),
-    requirements=["migen"]
-)
+You can install this package locally (in development mode) using:
+``$ pip install -e .``.
+"""
+from setuptools import find_packages
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="entangler",
+        version="0.1",
+        packages=find_packages(),
+        requirements=["migen"],
+        url="https://github.com/OxfordIonTrapGroup/entangler-core",
+        setup_requires=["pytest-runner"],
+        tests_require=["pytest"],
+    )
