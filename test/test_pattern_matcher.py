c1eaf60c91ad86d70d26ceedf0031c060c8c5a8f
Drew Risinger <drewrisinger@users.noreply.github.com>
2019-10-31 14:22:03 -0400

    tests: comprehensive pattern_matcher test
    Change pattern_matcher tests to test ALL possible patterns.
    This is unfortunately slow on my laptop, so could probably add old
    function as a simple quick-fail unit test.

--- test/test_pattern_matcher.py
+++ test/test_pattern_matcher.py
@@ -1,29 +1,52 @@
 """Test the :class:`entangler.core.PatternMatcher` properly pattern matches."""
+import itertools
+import logging
+
 from migen import run_simulation
 
 from entangler.core import PatternMatcher
 
-patterns = [0b1001, 0b0110, 0b1010, 0b0101]
 n_sig = 4
 
+_LOGGER = logging.getLogger(__name__)
 
-def pattern_match_test(dut):
+
+def test_match_one_pattern_set(dut, pattern_set, num_signals):
     """Test pattern recognition in the :class:`PatternMatcher`."""
-    for i, p in enumerate(patterns):
+    # set patterns to be matched to the given pattern_set
+    for i, p in enumerate(pattern_set):
         yield dut.patterns[i].eq(p)
     yield
 
-    for j in range(2 ** len(patterns)):
+    # check that is_match is only asserted on a valid pattern match
+    for j in range(2 ** len(pattern_set)):
         yield dut.pattern_ens.eq(j)
         yield
-        for i in range(2 ** n_sig):
+        for i in range(2 ** num_signals):
             yield dut.sig.eq(i)
             yield
             assert (yield dut.is_match) == any(
-                [p == i and (j & 2 ** n) for n, p in enumerate(patterns)]
+                (p == i and (j & 2 ** n) for n, p in enumerate(pattern_set))
             )
 
 
+def test_all_possible_patterns(dut, num_patterns):
+    """Test all possible pattern sets in the :class:`PatternMatcher`.
+
+    Args:
+        num_patterns: number of patterns that the ``dut`` can match against
+    """
+    all_possible_patterns = itertools.permutations(range(2 ** n_sig), num_patterns)
+
+    for pattern_set in all_possible_patterns:
+        _LOGGER.debug("Testing pattern: %s", pattern_set)
+        yield from test_match_one_pattern_set(dut, pattern_set, num_signals=n_sig)
+
+
 if __name__ == "__main__":
-    dut = PatternMatcher(num_inputs=n_sig, num_patterns=len(patterns))
-    run_simulation(dut, pattern_match_test(dut), vcd_name="heralder.vcd")
+    logging.basicConfig(level=logging.INFO)
+    num_patterns = n_sig
+    dut = PatternMatcher(num_inputs=n_sig, num_patterns=num_patterns)
+    run_simulation(
+        dut, test_all_possible_patterns(dut, num_patterns), vcd_name="heralder.vcd"
+    )
