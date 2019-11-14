43b10a0c38a53d5926ab9d7928e39ac9e39ff0cb
Drew Risinger <drewrisinger@users.noreply.github.com>
2019-11-14 11:41:26 -0500

    test utils: add wait_until method
    Allows waiting until a certain signal happens, should make testing
    easier.

--- test/helpers/gateware_utils.py
+++ test/helpers/gateware_utils.py
@@ -1,13 +1,40 @@
 """Shared functions & classes for testing the :mod:`entangler` gateware."""
+import logging
+
 from migen import If
 from migen import Module
 from migen import Signal
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def advance_clock(num_cycles: int) -> None:
     """Advance the simulation clock by some number of cycles."""
     for _ in range(num_cycles):
         yield
+
+
+def wait_until(
+    signal: Signal, wait_until_high: bool = True, max_cycles: int = None
+) -> None:
+    """Advance the simulation clock until signal goes high (==1).
+
+    If you provide ``max_cycles``, then it will timeout. If you set
+    ``wait_until_high=False``, then it will look for a low signal instead.
+    """
+    if max_cycles is None:
+        while (yield signal) != int(wait_until_high):
+            yield
+    else:
+        i = 0
+        while (yield signal) != int(wait_until_high) and i < max_cycles:
+            i += 1
+            yield
+        if i >= max_cycles:
+            _LOGGER.warning(
+                "Wait timed out after %i cycles, might not have waited long enough",
+                max_cycles,
+            )
 
 
 def rtio_output_event(rtlink, addr, data):
