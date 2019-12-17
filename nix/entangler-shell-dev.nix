f4f308868e309885d5c9e61047949da0d120cfb8
Drew Risinger <drisingr@umd.edu>
2019-12-17 18:32:36 -0500

    Nix Shell: notes on attempt to remove <artiqSrc>
    Couldn't remove, added notes on why not/what might be able to be done.

--- nix/entangler-shell-dev.nix
+++ nix/entangler-shell-dev.nix
@@ -4,7 +4,7 @@
 # Adds the Entangler package to ARTIQ's default "shell-dev.nix" environment
 
 # Can be called like ``$ nix-shell -I artiqSrc=/PATH/TO/ARTIQ/GIT/REPO ./entangler-shell-dev.nix``
-# TODO: remove dependency on artiqSrc
+# TODO: remove dependency on <artiqSrc>. Can't figure out how to easily remove, other than maybe patch.
 
 let
   entangler = pkgs.callPackage ./default.nix {};
@@ -12,9 +12,11 @@ let
     url="https://git.m-labs.hk/M-Labs/nix-scripts.git";
     ref="master"; # impure, but fine for our purposes
   };
-  dev-artiq-shell = pkgs.callPackage "${mlabs-nix-scripts}/artiq-fast/shell-dev.nix" {};  # Depends on artiqSrc
+  dev-artiq-shell = import "${mlabs-nix-scripts}/artiq-fast/shell-dev.nix" {};  # Depends on <artiqSrc> to import, can't remove artiqSrc dependency easily. moving on.
   # Force shell to use Release (i.e. MLabs Nix Channel) ARTIQ build, instead of passing all source/arguments ourselves.
-  dev-shell-with-release-artiq = dev-artiq-shell.overrideAttrs (oldAttrs: rec {artiqpkgs = pkgs.callPackage <artiq-full> {}; });
+  artiq-full = pkgs.callPackage <artiq-full> {};
+  patched-artiq-full = artiq-full.overrideAttrs (oldAttrs: rec {artiq = entangler.patched-artiq; });
+  dev-shell-with-release-artiq = dev-artiq-shell.overrideAttrs (oldAttrs: rec {artiqpkgs = patched-artiq-full; });
 in
   pkgs.mkShell{
     # Add Entangler to the development shell
