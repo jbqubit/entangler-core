b7ef361029da8ab245f4be254b15ab4a5c3a3513
Drew Risinger <drewrisinger@users.noreply.github.com>
2019-12-18 12:12:48 -0500

    test (IonPhoton): run test multiple times.
    Simulates real usage, just to confirm that the Entangler will still
    work properly when used multiple sequential times.

--- test/test_ion_photon.py
+++ test/test_ion_photon.py
@@ -123,6 +123,7 @@ def test_phy_basic(ip_phy: PhyTestHarness):
     _LOGGER.info("Starting basic IonPhoton EntanglerPHY functional test")
 
     def basic_phy_function(
+        num_runs: int = 2,
         pump_stop_time_ns: int = 1000,
         photon_window_ns: int = 50,
         num_failed_cycles: int = 9,
@@ -136,7 +137,8 @@ def test_phy_basic(ip_phy: PhyTestHarness):
         functioning as expected.
 
         Args:
-            phy (PhyTestHarness): The gateware simulation object to modify.
+            num_runs (int): Number of times to run the PHY. Confirms that the PHY
+                will work if run multiple times.
             pump_stop_time_ns (int, optional): Time in ns when pumping the experiment
                 should stop (assumes starts pumping at start of cycle, ends at this
                 time). Defaults to 1000.
@@ -172,111 +174,124 @@ def test_phy_basic(ip_phy: PhyTestHarness):
         if event_times_rel_to_pump_stop is None:
             event_times_rel_to_pump_stop = (0, photon_window_ns, -10, -30)
 
-        yield from ip_phy.write(
-            settings.ADDRESS_WRITE.CONFIG, 0b110
-        )  # disable, standalone
-        assert not bool((yield core.enable))
-        assert bool((yield msm.act_as_master))
-        assert bool((yield msm.standalone))
-
-        cycle_len_ns = pump_stop_time_ns + (photon_window_ns * 3)
-        pump_stop_time_coarse = math.ceil(pump_stop_time_ns / COARSE_CLOCK_PERIOD_NS)
-        pump_timing_coarse = (0, pump_stop_time_coarse)
-
-        for i, seq in enumerate(core.sequencers):
+        for run in range(num_runs):
+            _LOGGER.debug("Starting run %i", run + 1)
             yield from ip_phy.write(
-                ADDR_TIMING + i, (pump_timing_coarse[1] << 16) | pump_timing_coarse[0]
+                settings.ADDRESS_WRITE.CONFIG, 0b110
+            )  # disable, standalone
+            assert not bool((yield core.enable))
+            assert bool((yield msm.act_as_master))
+            assert bool((yield msm.standalone))
+
+            cycle_len_ns = pump_stop_time_ns + (photon_window_ns * 3)
+            pump_stop_time_coarse = math.ceil(
+                pump_stop_time_ns / COARSE_CLOCK_PERIOD_NS
             )
-            assert (yield seq.m_start) == pump_timing_coarse[0]
-            assert (yield seq.m_stop) == pump_timing_coarse[1]
+            pump_timing_coarse = (0, pump_stop_time_coarse)
 
-        photon_valid_window = (pump_stop_time_ns, pump_stop_time_ns + photon_window_ns)
+            for i, seq in enumerate(core.sequencers):
+                yield from ip_phy.write(
+                    ADDR_TIMING + i,
+                    (pump_timing_coarse[1] << 16) | pump_timing_coarse[0],
+                )
+                assert (yield seq.m_start) == pump_timing_coarse[0]
+                assert (yield seq.m_stop) == pump_timing_coarse[1]
 
-        num_sequencers = settings.NUM_OUTPUT_CHANNELS
-        assert num_sequencers == len(core.sequencers)
-        for i, gater in enumerate(core.apd_gaters):
-            write_addr = ADDR_TIMING + num_sequencers + i
-            yield from ip_phy.write(
-                write_addr, photon_valid_window[1] << 16 | photon_valid_window[0]
+            photon_valid_window = (
+                pump_stop_time_ns,
+                pump_stop_time_ns + photon_window_ns,
             )
-            assert (yield gater.gate_start) == photon_valid_window[0]
-            assert (yield gater.gate_stop) == photon_valid_window[1]
 
-        herald_patterns = ION_PHOTON_HERALD_PATTERNS
-        yield from ip_phy.write_heralds(herald_patterns)
-        assert (yield core.heralder.pattern_ens) == 2 ** len(herald_patterns) - 1
-        for i, pattern in enumerate(herald_patterns):
-            assert (yield core.heralder.patterns[i]) == pattern
-        assert not bool((yield core.heralder.is_match))
+            num_sequencers = settings.NUM_OUTPUT_CHANNELS
+            assert num_sequencers == len(core.sequencers)
+            for i, gater in enumerate(core.apd_gaters):
+                write_addr = ADDR_TIMING + num_sequencers + i
+                yield from ip_phy.write(
+                    write_addr, photon_valid_window[1] << 16 | photon_valid_window[0]
+                )
+                assert (yield gater.gate_start) == photon_valid_window[0]
+                assert (yield gater.gate_stop) == photon_valid_window[1]
 
-        yield from ip_phy.write(settings.ADDRESS_WRITE.CONFIG, 0b111)
-        assert bool((yield core.enable))
-        assert bool((yield msm.is_master))
-        assert bool((yield msm.standalone))
+            herald_patterns = ION_PHOTON_HERALD_PATTERNS
+            yield from ip_phy.write_heralds(herald_patterns)
+            assert (yield core.heralder.pattern_ens) == 2 ** len(herald_patterns) - 1
+            for i, pattern in enumerate(herald_patterns):
+                assert (yield core.heralder.patterns[i]) == pattern
+            if run == 0:
+                assert not bool((yield core.heralder.is_match))
 
-        cycle_len_coarse = int(cycle_len_ns / COARSE_CLOCK_PERIOD_NS)
-        yield from ip_phy.write(settings.ADDRESS_WRITE.TCYCLE, cycle_len_coarse)
-        assert (yield msm.cycle_length_input) == cycle_len_coarse
+            yield from ip_phy.write(settings.ADDRESS_WRITE.CONFIG, 0b111)
+            assert bool((yield core.enable))
+            assert bool((yield msm.is_master))
+            assert bool((yield msm.standalone))
 
-        runtime = cycle_len_coarse * cycles_until_timeout
-        max_clk_per_cycle = cycle_len_coarse + 5
-        yield from ip_phy.write(settings.ADDRESS_WRITE.RUN, runtime)
-        for _ in range(num_failed_cycles):
-            yield from wait_until(msm.cycle_ending, max_cycles=max_clk_per_cycle)
-            assert not bool((yield msm.success))
-            assert bool((yield msm.cycle_ending))
-            yield
+            cycle_len_coarse = int(cycle_len_ns / COARSE_CLOCK_PERIOD_NS)
+            yield from ip_phy.write(settings.ADDRESS_WRITE.TCYCLE, cycle_len_coarse)
+            assert (yield msm.cycle_length_input) == cycle_len_coarse
 
-        # trigger first herald match
-        yield from ip_phy.set_event_times(
-            pump_stop_time_ns, event_times_rel_to_pump_stop
-        )
+            runtime = cycle_len_coarse * cycles_until_timeout
+            max_clk_per_cycle = cycle_len_coarse + 5
+            yield from ip_phy.write(settings.ADDRESS_WRITE.RUN, runtime)
+            yield from wait_until(msm.cycle_starting, max_cycles=10)
+            # reset event times to 0, instead of assuming they start at 0
+            yield from ip_phy.set_event_times(0, [0] * len(core.apd_gaters))
+            for _ in range(num_failed_cycles):
+                yield from wait_until(msm.cycle_ending, max_cycles=max_clk_per_cycle)
+                assert bool((yield msm.cycle_ending))
+                assert not bool((yield msm.success))
+                yield
 
-        yield from wait_until(msm.done_stb, max_cycles=max_clk_per_cycle)
+            # trigger first herald match
+            yield from ip_phy.set_event_times(
+                pump_stop_time_ns, event_times_rel_to_pump_stop
+            )
 
-        assert bool((yield msm.success))
-        # check matched the correct pattern
-        assert (yield ip_phy.core.rtlink.i.data) == 0b1000
-        assert (yield msm.cycles_completed) == num_failed_cycles + 1
+            yield from wait_until(msm.done_stb, max_cycles=max_clk_per_cycle)
 
-        # pass lists as buffers to HACK-return values from yield-from func
-        cyc_complete = [0]
-        status = [0]
-        time_remaining = [0]
-        triggers = [0]
-        timestamps = [[0]] * settings.NUM_INPUT_SIGNALS
+            assert bool((yield msm.success))
+            # check matched the correct pattern
+            assert (yield ip_phy.core.rtlink.i.data) == 0b1000
+            assert (yield msm.cycles_completed) == num_failed_cycles + 1
 
-        yield from ip_phy.read(settings.ADDRESS_READ.TIME_REMAINING, time_remaining)
-        _LOGGER.debug("Time remaining: %i", time_remaining[0])
-        yield from ip_phy.read(settings.ADDRESS_READ.NCYCLES, cyc_complete)
-        yield from ip_phy.read(settings.ADDRESS_READ.STATUS, status)
-        _LOGGER.debug("Status: %i", status[0])
-        yield from ip_phy.read(settings.ADDRESS_READ.NTRIGGERS, triggers)
-        _LOGGER.debug("Num triggers: %i", triggers[0])
-        for i, ts in enumerate(timestamps):
-            yield from ip_phy.read(settings.ADDRESS_READ.TIMESTAMP + i, ts)
-            _LOGGER.debug("Read timestamp[%i]: %i", i, ts[0])
-            if 0 <= event_times_rel_to_pump_stop[i] <= photon_window_ns:
-                # valid arrival times
-                assert ts[0] == event_times_rel_to_pump_stop[i] + pump_stop_time_ns
-            else:
-                assert ts[0] == 0
+            # pass lists as buffers to HACK-return values from yield-from func
+            cyc_complete = [0]
+            status = [0]
+            time_remaining = [0]
+            triggers = [0]
+            timestamps = [[0]] * settings.NUM_INPUT_SIGNALS
 
-        assert cyc_complete[0] == num_failed_cycles + 1
-        assert status[0] == 0b010  # not ready (i.e. starting), success, not timeout
-        assert triggers[0] == 0
-        # 4 for extra states in state machine (starting/stopping, etc)
-        expected_time_elapsed = cyc_complete[0] * (cycle_len_coarse + 4)
-        # Bound runtime to a fairly close number of cycles.
-        assert (
-            (runtime - expected_time_elapsed - 5)
-            < time_remaining[0]
-            < (runtime - expected_time_elapsed)
-        )
+            yield from ip_phy.read(settings.ADDRESS_READ.TIME_REMAINING, time_remaining)
+            _LOGGER.debug("Time remaining: %i", time_remaining[0])
+            yield from ip_phy.read(settings.ADDRESS_READ.NCYCLES, cyc_complete)
+            yield from ip_phy.read(settings.ADDRESS_READ.STATUS, status)
+            _LOGGER.debug("Status: %i", status[0])
+            yield from ip_phy.read(settings.ADDRESS_READ.NTRIGGERS, triggers)
+            _LOGGER.debug("Num triggers: %i", triggers[0])
+            for i, ts in enumerate(timestamps):
+                yield from ip_phy.read(settings.ADDRESS_READ.TIMESTAMP + i, ts)
+                _LOGGER.debug("Read timestamp[%i]: %i", i, ts[0])
+                if 0 <= event_times_rel_to_pump_stop[i] <= photon_window_ns:
+                    # valid arrival times
+                    assert ts[0] == event_times_rel_to_pump_stop[i] + pump_stop_time_ns
+                else:
+                    assert ts[0] == 0
+
+            assert cyc_complete[0] == num_failed_cycles + 1
+            assert status[0] == 0b010  # not ready (i.e. starting), success, not timeout
+            assert triggers[0] == 0
+            # 4 for extra states in state machine (starting/stopping, etc)
+            expected_time_elapsed = cyc_complete[0] * (cycle_len_coarse + 4)
+            # Bound runtime to a fairly close number of cycles.
+            assert (
+                (runtime - expected_time_elapsed - 5)
+                < time_remaining[0]
+                < (runtime - expected_time_elapsed)
+            )
 
     migen.run_simulation(
         ip_phy,
         basic_phy_function(
+            num_runs=3,
             pump_stop_time_ns=1000,
             photon_window_ns=50,
             cycles_until_timeout=40,
@@ -284,5 +299,5 @@ def test_phy_basic(ip_phy: PhyTestHarness):
             event_times_rel_to_pump_stop=(0, 50, -10, -30),  # pattern 1100
         ),
         vcd_name="ion_photon_phy.vcd",
-        clocks={name: COARSE_CLOCK_PERIOD_NS for name in ["rio", "sys", "rio_phy"]},
+        clocks={name: COARSE_CLOCK_PERIOD_NS for name in ("rio", "sys", "rio_phy")},
     )
